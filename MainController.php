901e2b6f7146d5a86649e9cd492a8c136c0cc40d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-09-10 01:44:16 +0900

    コンフリクト修正

--- MainController.php
+++ MainController.php
@@ -1,7 +1,4 @@
 <?php
-
-require_once './vendor/autoload.php';
-
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Chrome\ChromeDriver;
@@ -9,47 +6,28 @@ use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use Facebook\WebDriver\WebDriverBy;
 
+/**
+ * selenium facebook-webdriver 実行のサンプル
+ */
 class Main {
-    const SELENIUM_TIMEOUT_SEC = 800; // seleniumのタイムアウト時間(秒)
+    const SELENIUM_TIMEOUT_SEC = 60; // seleniumのタイムアウト時間(秒)
 
-    /**
-     * selenium facebook-webdriver 実行のサンプル
-     */
     function run() {
-        $driver = $this->createDriver(); // オプション起動(headless等)
-        // 指定URLへ遷移 (Google)
+        $driver = $this->createDriver(); 
         $driver->get('https://www.google.co.jp/');
-        // 検索Box
         $element = $driver->findElement(WebDriverBy::name('q'));
-        // 検索Boxにキーワードを入力して
         $element->sendKeys('セレニウムで自動操作');
-        // 検索実行
         $element->submit();
 
-        // 検索結果画面のタイトルが 'セレニウムで自動操作 - Google 検索' になるまで10秒間待機する
-        // 指定したタイトルにならずに10秒以上経ったら
-        // 'Facebook\WebDriver\Exception\TimeOutException' がthrowされる
-        $driver->wait(10)->until(
-            WebDriverExpectedCondition::titleIs('セレニウムで自動操作 - Google 検索')
-        );
-
-        // セレニウムで自動操作 - Google 検索 というタイトルを取得できることを確認する
-        if ($driver->getTitle() !== 'セレニウムで自動操作 - Google 検索') {
-            throw new Exception('fail');
-        }
         // キャプチャ
         $file = __DIR__ . '/sample_chrome.png';
         $driver->takeScreenshot($file);
-        echo $file;
-        echo '<img src="./sample_chrome.png">';
+        echo '<img src="'.$file.'">';
 
-        // ブラウザを閉じる
         $driver->close();
     }
 
-
     private function createDriver() {
-
         $options = new ChromeOptions();
         $options->addArguments(array(
             '--headless',
@@ -61,6 +39,7 @@ class Main {
             putenv('webdriver.chrome.driver=/app/.chromedriver/bin/chromedriver');
             $options->setBinary(getenv('GOOGLE_CHROME_SHIM'));
         }else {
+            // ローカルでwhich chromedriverとコマンドを打って表示されるPATH
             putenv('webdriver.chrome.driver=/usr/local/bin/chromedriver');
         }
         $capabilities = DesiredCapabilities::chrome();
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once './vendor/autoload.php';
 require_once dirname(__FILE__) . '/MainController.php';
 
 $main = new Main();
